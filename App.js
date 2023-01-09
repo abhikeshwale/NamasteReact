651354e0fb97df9c629d81c93f850d8f03110418
abhikeshwale <abhikeshwale@gmail.com>
2023-01-09 16:19:18 +0900

    Chapter 02 Commit

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 const heading1 = React.createElement(
   "h1",
-  {},
+  { id: "title2", key: "t2" },
   "This is Heading 1 from the react CreateElement Tag"
 );
+const hed = () => {
+  return <div></div>;
+};
+
 const heading2 = React.createElement(
   "h1",
-  {},
+  { id: "title1", key: "t1" },
   "This is Heading 2 from the react CreateElement Tag"
 );
 
